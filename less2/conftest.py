d66311d6081bf93710ba4239dac4cbff6d40889e
stas <stasma@gmail.com>
2019-03-30 21:08:27 +0300

    Added conftest.py

--- less2/conftest.py
+++ less2/conftest.py
@@ -1,14 +1,30 @@
+"""Conftest"""
+
 import pytest
+
+
+@pytest.fixture(scope="session", autouse=True)
+def start_of_session(request):
+    print('\n *** Start of run: The session is began *** ')
+
+    def enf_of_session():
+        print('\n *** End of run: The session is ended *** ')
+    request.addfinalizer(enf_of_session)
+
 
 @pytest.fixture()
 def setup():
-    print("\n Running conftest method setUP")
+    print("Running conftest method setUP")
+    print("-"*30)
     yield
-    print("\n Running conftest method tearDown")
+    print("\nRunning conftest method tearDown")
+    print("-" * 30)
 
 
 @pytest.fixture(scope="module")
 def onetimesetup():
-    print("\n Running conftest one time setUP")
+    print("\nRunning conftest one time setUP")
+    print("-" * 30)
     yield
-    print("\n Running conftest one time tearDown")
+    print("Running conftest one time tearDown")
+    print("-" * 30)
