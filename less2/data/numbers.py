213bc5741d760dba06270c2264dc5c1ca3017a1f
stas <stasma@gmail.com>
2019-03-30 21:11:19 +0300

    Data folder update

--- less2/data/numbers.py
+++ less2/data/numbers.py
@@ -1,15 +1,24 @@
-class Math():
+class Math(object):
 
-    # def __init__(self, num1, num2):
-    #     self.num1 = num1
-    #     self.num2 = num2
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
 
-
-    def sum_num(num1, num2):
+    def sum_nums(self):
         """
         :type nums are: int
-        :param self.num1:
+        :param num1:
         :param num2:
         :return sum of params:
         """
-        return num1 + num2
+        return self.num1 + self.num2
+
+    def sub_nums(self):
+        """
+        :type nums are: int
+        :param num1:
+        :param num2:
+        :return sum of params:
+        """
+        return self.num1 - self.num2
+
--- less2/data/strings.py
+++ less2/data/strings.py
@@ -1,4 +1,23 @@
-
+""" Function"""
 def join_strings(word1, word2):
-        return word1 + ' ' + word2
+    """
+    :param word1:
+    :param word2:
+    :return sum:
+    """
+    return word1 + ' ' + word2
 
+def uplowcase(string):
+    """
+    :param string:
+    :return:
+    """
+    upcase = 0
+    lowcase = 0
+    for i in string:
+        if i.isupper():
+            upcase = upcase + 1
+        elif i.islower():
+            lowcase = lowcase + 1
+
+    return (upcase, lowcase)
